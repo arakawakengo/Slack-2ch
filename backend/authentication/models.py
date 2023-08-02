854b3a1869e54558743b3d85bc824d90cd47a74d
arakawakengo <70435005+arakawakengo@users.noreply.github.com>
2023-08-02 09:58:05 +0900

    モデルの制約の追加

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,8 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Permission, Group
-import secrets
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
+class Workspace(models.Model):
+    w_id = models.AutoField(primary_key=True)
+    workspace_id = models.CharField(max_length=20, unique=True)
+    workspace_name = models.CharField(max_length=20)
+    workspace_token = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.workspace_name)
 
 class CustomUser(AbstractUser):
+    
     groups = models.ManyToManyField(
         Group,
         verbose_name= ('groups'),
@@ -24,17 +33,21 @@ class CustomUser(AbstractUser):
         related_query_name="%(app_label)s_%(class)ss",
     )
     
-    u_ID = models.AutoField(primary_key=True)
+    workspace = models.ForeignKey(
+        Workspace,
+        on_delete=models.CASCADE,
+        related_name='users'
+    )
+    
     user_id = models.CharField(max_length=20)
-    workspace_id = models.CharField(max_length=20)
     username = models.CharField(max_length=20)
     channel_id = models.CharField(max_length=20)
     
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user_id", "workspace"],
+                name="user_unique"
+            ),
+        ]
     
-class Workspace(models.Model):
-    w_id = models.AutoField(primary_key=True)
-    workspace_id = models.CharField(max_length=20)
-    workspace_name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return str(self.workspace_name)
