6bfa9ddf71ed8dc9926d0d1156e641723f167fdb
arakawakengo <70435005+arakawakengo@users.noreply.github.com>
2023-08-02 12:32:25 +0900

    色々たくさん修正

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,33 +1,60 @@
 from django.db import models
+from authentication.models import CustomUser
+from django.utils import timezone
     
 class dbposts(models.Model):
     id  = models.AutoField(primary_key=True)
-    user_id = models.CharField(max_length=100)
-    user_name = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='users_posts'
+    )
     text = models.CharField(max_length=100)
     category = models.CharField(max_length=10)
-    comment_cnt = models.IntegerField()
-    created_at = models.DateTimeField()
+    comment_cnt = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.text
     
 class dbquestions(models.Model):
     id = models.AutoField(primary_key=True)
-    # user_id = models.CharField(max_length=100)
-    post_id = models.IntegerField()
+    post = models.ForeignKey(
+        dbposts,
+        on_delete=models.CASCADE,
+        related_name='posts_questions'
+    )
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='users_questions'
+    )
     text = models.CharField(max_length=100)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(default=timezone.now)
+    
+    
 
     def __str__(self):
         return self.text
 
 class dbreplies(models.Model):
     id = models.AutoField(primary_key=True)
-    # user_id = models.CharField(max_length=100)
-    question_id = models.IntegerField()
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='users_replies'
+    )
+    question = models.ForeignKey(
+        dbquestions,
+        on_delete=models.CASCADE,
+        related_name='questions_replies',
+    )
     text = models.CharField(max_length=100)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.text
+
