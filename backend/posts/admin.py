a1a34168a73ba55b7bc010a76a184c57f79efc2b
prerin <yukimuraryofu@icloud.com>
2023-08-03 13:44:19 +0900

    複数人にdm login時の返り値の変更

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # モデルをインポート
-from .models import dbposts, dbquestions, dbreplies
+from .models import Posts, Questions, Replies
 
 class PostsAdmin(admin.ModelAdmin):
     list_display = ('id', "user_id", 'text', "comment_cnt", "created_at")
@@ -13,6 +13,6 @@ class RepliesAdmin(admin.ModelAdmin):
     list_display = ('id', "user", "question", 'text', "created_at")
 
 # 管理ツールに登録
-admin.site.register(dbposts, PostsAdmin)
-admin.site.register(dbquestions, QuestionsAdmin)
-admin.site.register(dbreplies, RepliesAdmin)
+admin.site.register(Posts, PostsAdmin)
+admin.site.register(Questions, QuestionsAdmin)
+admin.site.register(Replies, RepliesAdmin)
